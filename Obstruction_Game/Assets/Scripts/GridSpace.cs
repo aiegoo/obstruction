c88b16f7c3e991b90654dccc78dce0fb807b87e3
aiegoo <eozz21@gmail.com>
2020-11-21 12:49:30 +0900

    add scene, prefab and scripts

---
+++ Obstruction_Game/Assets/Scripts/GridSpace.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour
+{
+
+    public Button button;
+    public Text buttonText;
+    public string playerSymbol;
+
+    public void buttonClick()
+    {
+
+        playerSymbol = "X";
+        buttonText.text = playerSymbol;
+        button.interactable = false;
+    }
+
+}
